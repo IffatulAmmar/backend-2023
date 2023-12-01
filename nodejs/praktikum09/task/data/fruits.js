dd8bbcf70bc01b17732e99d842b3bdd17794c099
IffatulAmmar <m.iffatul.a@gmail.com>
2023-12-01 10:36:21 +0700

    pertemuan 9

---
+++ nodejs/praktikum09/task/data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["melon","pisang","pepaya","semangka"];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
